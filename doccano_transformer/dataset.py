e22125fe9b87b89bcfaedbd91d310f732a69d76a
yasufumi <yasufumi.taniguchi@gmail.com>
2020-04-07 16:06:30 +0900

    Add minumul codes for Dataset.to_conll2003

--- doccano_transformer/dataset.py
+++ doccano_transformer/dataset.py
@@ -1,6 +1,8 @@
+import os
 from typing import Callable, List, Optional
 
 from . import utils
+from .formats import NotSupportedOutputFormatError, OutputFormat
 from .tasks import Task
 
 
@@ -9,18 +11,28 @@ class Dataset:
                  filepath: str,
                  task: Task,
                  tokenizer: Optional[Callable[[str], List[str]]] = str.split):
+        """
+            Args:
+                filepath (str): The path to the exported Doccano file.
+                task (Task): The annotation task of the exported file.
+                tokenizer (Optional[Callable[[str], List[str]]]): The tokenizer.
+        """
 
-        utils.check_exists(filepath)
-
-        if utils.is_valid_jsonl(filepath):
-            self.data = utils.from_jsonl(filepath)
-        elif utils.is_valid_csv(filepath):
-            self.data = utils.from_csv(filepath)
-        else:
-            raise utils.NotDoccanoFormatError
+        if not os.path.isfile(filepath):
+            raise FileNotFoundError
 
         self.task = task
+        self.data = task.load(filepath)
         self.tokenizer = tokenizer
 
     def to_conll2003(self, savepath: str):
-        ...
+        """
+            Args:
+                savepath (str): The path to save the file converted to
+                    CoNLL2003 format.
+        """
+        if OutputFormat.CoNLL2003 not in self.task.allowed_output_formats:
+            raise NotSupportedOutputFormatError
+        else:
+            converted_data = self.data.to_conll2003()
+            utils.save_to_text(converted_data, savepath)
--- doccano_transformer/tasks.py
+++ doccano_transformer/tasks.py
@@ -1,19 +1,34 @@
-import json
-import os
+from typing import List, NamedTuple
+
+from . import formats, utils
 
 
 class Task:
+
     @staticmethod
-    def check_format(filepath: str) -> bool:
+    def load(filepath: str) -> List[NamedTuple]:
         raise NotImplementedError
 
 
 class NER(Task):
-    @staticmethod
-    def check_format(filepath: str) -> bool:
+
+    allowed_output_formats = {
+        formats.OutputFormat.CoNLL2003,
+        formats.OutputFormat.SpaCy
+    }
+
+    def __init__(self, data: List[dict]) -> None:
         try:
-            with open(filepath) as f:
-                for line in f:
-                    json.loads(line.rstrip(os.linesep))
-        except json.JSONDecodeError as e:
-            raise e
+            self.data = [formats.NERFormat(**x) for x in data]
+        except TypeError:
+            self.data = [formats.NERTextLabelFormat(**x) for x in data]
+        except TypeError:
+            raise formats.NotSupportedInputFormatError
+
+    @classmethod
+    def load(cls, filepath: str) -> List[NamedTuple]:
+        raw_data = utils.load_from_jsonl(filepath)
+        return cls(raw_data)
+
+    def to_conll2003(self) -> List[str]:
+        return [x.to_conll2003() for x in self.data]
