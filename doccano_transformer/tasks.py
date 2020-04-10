5234907d7fa5e0466ffb6c606c4d46dcf4863a08
yasufumi <yasufumi.taniguchi@gmail.com>
2020-04-10 15:25:19 +0900

    Update Dataset and tasks.NER

--- doccano_transformer/tasks.py
+++ doccano_transformer/tasks.py
@@ -1,4 +1,4 @@
-from typing import List, NamedTuple
+from typing import Callable, List, Optional
 
 from . import formats, utils
 
@@ -6,7 +6,7 @@ from . import formats, utils
 class Task:
 
     @staticmethod
-    def load(filepath: str) -> List[NamedTuple]:
+    def load(filepath: str) -> 'Task':
         raise NotImplementedError
 
 
@@ -17,18 +17,29 @@ class NER(Task):
         formats.OutputFormat.SpaCy
     }
 
-    def __init__(self, data: List[dict]) -> None:
-        try:
-            self.data = [formats.NERFormat(**x) for x in data]
-        except TypeError:
-            self.data = [formats.NERTextLabelFormat(**x) for x in data]
-        except TypeError:
+    def __init__(
+        self, data: List[dict], tokenizer: Callable[[str], List[str]]
+    ) -> None:
+        if all(formats.NER.valid(x) for x in data):
+            self.data = [formats.NER(x, tokenizer) for x in data]
+        elif all(formats.NERTextLabel.valid(x) for x in data):
+            self.data = [formats.NERTextLabel(x, tokenizer) for x in data]
+        else:
             raise formats.NotSupportedInputFormatError
 
     @classmethod
-    def load(cls, filepath: str) -> List[NamedTuple]:
-        raw_data = utils.load_from_jsonl(filepath)
-        return cls(raw_data)
+    def load(cls,
+             filepath: str,
+             tokenizer: Callable[[str], List[str]]
+             ) -> 'NER':
+        raw_data: List[dict] = utils.load_from_jsonl(filepath)
+        return cls(raw_data, tokenizer)
 
-    def to_conll2003(self) -> List[str]:
-        return [x.to_conll2003() for x in self.data]
+    def to_conll2003(self, user: Optional[int] = None) -> List[str]:
+        result = ['-DOCSTART- -X- -X- O\n\n']
+        for x in self.data:
+            line = x.to_conll2003(user)
+            if not line:
+                continue
+            result.append(line)
+        return result
