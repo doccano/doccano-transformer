6c893308f03fce170bc5fbe2c550b601df8e2f5f
yasufumi <yasufumi.taniguchi@gmail.com>
2020-04-10 15:24:58 +0900

    Update doccano_transformer.formats

--- doccano_transformer/formats.py
+++ doccano_transformer/formats.py
@@ -1,6 +1,8 @@
-import os
-from collections import namedtuple
+from collections import defaultdict
 from enum import Enum
+from typing import Callable, List, Optional
+
+from doccano_transformer import utils
 
 
 class NotSupportedInputFormatError(Exception):
@@ -11,37 +13,78 @@ class NotSupportedOutputFormatError(Exception):
     pass
 
 
-class NERFormat(namedtuple(
-    'NERFormat',
-    ['id', 'text', 'meta', 'annotations', 'annotation_approver']
-)):
+class InputFormat:
 
-    def to_conll2003(self):
-        annotations = self.annotations
-        text = self.text
+    @staticmethod
+    def valid(x: dict) -> bool:
+        raise NotImplementedError
+
+
+class NER(InputFormat):
+
+    FIELDS = ['id', 'text', 'meta', 'annotations', 'annotation_approver']
+
+    @classmethod
+    def valid(cls, x: dict) -> bool:
+        # TODO: This method should be stricter.
+        for field in cls.FIELDS:
+            if field not in x:
+                return False
+        return True
+
+    def __init__(self, x: dict, tokenizer: Callable[[str], List[str]]) -> None:
+        if not self.valid(x):
+            raise NotSupportedInputFormatError
+
+        self.id = x['id']
+        self.text = x['text']
+        self.tokens = tokenizer(x['text'])
+        self.offsets = utils.get_offsets(self.text, self.tokens)
+        self.meta = x['meta']
+        labels = defaultdict(list)
+        for annotation in x['annotations']:
+            labels[annotation['user']].append([
+                annotation['start_offset'],
+                annotation['end_offset'],
+                annotation['label']
+            ])
+        self.labels = labels
+        self.annotation_approver = x['annotation_approver']
+        self.default_user = max(labels.keys(), key=lambda user: len(labels[user]))
+
+    def to_conll2003(self, user: Optional[int] = None) -> str:
+        label = self.labels[user or self.default_user]
+        if not label:
+            return None
+        tags = utils.create_bio_tags(self.tokens, self.offsets, label)
         lines = []
-        for annotation in annotations:
-            start_offset = annotation['start_offset']
-            end_offset = annotation['end_offset']
-            token = text[start_offset: end_offset]
-            tag = annotation['label']
-            lines.append(f'{token}\t{tag}{os.linesep}')
-        return ''.join(lines) + os.linesep
+        for token, tag in zip(self.tokens, tags):
+            lines.append(f'{token} _ _ {tag}\n')
+        lines.append('\n')
+        return ''.join(lines)
 
 
-class NERTextLabelFormat(namedtuple(
-    'NERTextLabelFormat',
-    ['id', 'text', 'meta', 'labels', 'annotation_approver']
-)):
+class NERTextLabel(NER):
 
-    def to_conll2003(self):
-        labels = self.labels
-        text = self.text
-        lines = []
-        for start, end, tag in labels:
-            token = text[start: end]
-            lines.append(f'{token}\t{tag}{os.linesep}')
-        return ''.join(lines) + os.linesep
+    FIELDS = ['id', 'text', 'meta', 'labels', 'annotation_approver']
+
+    def __init__(self, x: dict, tokenizer: Callable[[str], List[str]]) -> None:
+        if not self.valid(x):
+            raise NotSupportedInputFormatError
+
+        self.id = x['id']
+        self.text = x['text']
+        self.tokens = tokenizer(x['text'])
+        self.offsets = utils.get_offsets(self.text, self.tokens)
+        self.meta = x['meta']
+        labels = defaultdict(list)
+        for label in x['labels']:
+            # TODO: This format doesn't have a user field currently.
+            # So this method uses the user 0 for all label.
+            labels[-1].append(label)
+        self.labels = labels
+        self.annotation_approver = x['annotation_approver']
+        self.default_user = -1
 
 
 class OutputFormat(Enum):
--- tests/test_formats.py
+++ tests/test_formats.py
@@ -3,19 +3,42 @@ from unittest import TestCase
 from doccano_transformer import formats
 
 
-class TestNERFormat(TestCase):
+class TestNER(TestCase):
+
+    def test_valid_returns_true(self):
+        x = {'id': 0, 'text': 'Nadim Ladki', 'meta': {},
+             'annotation_approver': {},
+             'annotations': [
+            {'label': 2, 'start_offset': 0, 'end_offset': 5, 'user': 1},
+            {'label': 4, 'start_offset': 6, 'end_offset': 11, 'user': 2}
+        ]}
+        self.assertTrue(formats.NER.valid(x))
+
+    def test_valid_returns_false(self):
+        x = {'id': 0, 'text': 'Nadim Ladki', 'meta': {},
+             'annotation_approver': {}}
+        self.assertFalse(formats.NER.valid(x))
 
     def test_to_conll2003(self):
-        text = 'Nadim Ladki'
-        annotations = [
+        x = {'id': 0, 'text': 'Nadim Ladki', 'meta': {},
+             'annotation_approver': {},
+             'annotations': [
             {'label': 2, 'start_offset': 0, 'end_offset': 5, 'user': 1},
-            {'label': 4, 'start_offset': 6, 'end_offset': 11, 'user': 1}
-        ]
+            {'label': 4, 'start_offset': 6, 'end_offset': 11, 'user': 2}
+        ]}
+        result = formats.NER(x, str.split).to_conll2003(user=1)
+        expected = 'Nadim _ _ B-2\nLadki _ _ O\n\n'
+        self.assertEqual(result, expected)
 
-        x = formats.NERFormat(
-            id=0, text=text, annotations=annotations,
-            meta={}, annotation_approver={}
-        )
-        expected = 'Nadim\t2\nLadki\t4\n\n'
+        result = formats.NER(x, str.split).to_conll2003(user=2)
+        expected = 'Nadim _ _ O\nLadki _ _ B-4\n\n'
+        self.assertEqual(result, expected)
 
-        self.assertEqual(x.to_conll2003(), expected)
+
+class TestNERTextLabel(TestCase):
+    def test_to_conll2003(self):
+        x = {"id": 2576, 'text': 'Nadim Ladki', 'meta': {},
+             'annotation_approver': {}, 'labels': [[0, 11, 'PER']]}
+        result = formats.NERTextLabel(x, str.split).to_conll2003(user=-1)
+        expected = 'Nadim _ _ B-PER\nLadki _ _ I-PER\n\n'
+        self.assertEqual(result, expected)
