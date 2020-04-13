3e79074d842da07345d13f6a6658dca60a146e4e
yasufumi <yasufumi.taniguchi@gmail.com>
2020-04-13 15:50:20 +0900

    Update test cases for doccano_transformer.Dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,4 +1,3 @@
-import os
 from unittest import TestCase
 
 import pytest
@@ -14,10 +13,24 @@ class TestDataset(TestCase):
         self.shared_datadir = shared_datadir
         self.tmpdir = tmpdir
 
-    def test_to_conll2003(self):
+    def test_from_labeling_text_label_jsonl_to_conll2003(self):
         src_path = self.shared_datadir / 'labeling_text_label.jsonl'
-        tgt_path = self.tmpdir / 'labeling_text_label.conll'
+        tgt_path = self.tmpdir / 'labeling_text_label.conll2003'
+        expected = list(open(
+            self.shared_datadir / 'labeling_text_label.conll2003'
+        ))
         d = Dataset(filepath=src_path, task=NER)
         d.to_conll2003(tgt_path)
 
-        self.assertTrue(os.path.isfile(tgt_path))
+        self.assertListEqual(list(open(tgt_path)), expected)
+
+    def test_from_labeling_jsonl_to_conll2003(self):
+        src_path = self.shared_datadir / 'labeling.jsonl'
+        tgt_path = self.tmpdir / 'labeling.conll2003'
+        expected = list(open(
+            self.shared_datadir / 'labeling.conll2003'
+        ))
+        d = Dataset(filepath=src_path, task=NER)
+        d.to_conll2003(tgt_path)
+
+        self.assertListEqual(list(open(tgt_path)), expected)
