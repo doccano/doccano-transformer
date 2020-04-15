58800811e53079f2b23359393b68e7fcdc2b737e
yasufumi <yasufumi.taniguchi@gmail.com>
2020-04-15 14:04:56 +0900

    Add a test case for jsonl including multiple users

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 
 import pytest
-
 from doccano_transformer import Dataset
 from doccano_transformer.tasks import NER
 
@@ -23,6 +22,20 @@ class TestDataset(TestCase):
         d.to_conll2003(tgt_path)
 
         self.assertListEqual(list(open(tgt_path)), expected)
+
+    def test_from_labeling_multi_users_jsonl_to_conll2003(self):
+        src_path = self.shared_datadir / 'labeling_multi_users.jsonl'
+        filename = 'labeling_multi_users.conll2003'
+        tgt_path = self.tmpdir / filename
+        expected = sorted(self.shared_datadir.glob(f'{filename}.*'))
+
+        d = Dataset(filepath=src_path, task=NER)
+        d.to_conll2003(tgt_path)
+        for y in expected:
+            self.assertEqual(
+                (self.tmpdir / y.name).read_text(None),
+                y.read_text()
+            )
 
     def test_from_labeling_jsonl_to_conll2003(self):
         src_path = self.shared_datadir / 'labeling.jsonl'
