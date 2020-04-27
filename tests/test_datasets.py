5713d950424a9fb6125d90f0c34638ff11118165
yasufumi <yasufumi.taniguchi@gmail.com>
2020-04-27 11:29:14 +0900

    Update test cases

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,3 +1,5 @@
+import json
+from collections import defaultdict
 from unittest import TestCase
 
 import pytest
@@ -8,45 +10,58 @@ from doccano_transformer.datasets import NERDataset
 class TestNERDataset(TestCase):
 
     @pytest.fixture(autouse=True)
-    def initdir(self, shared_datadir, tmpdir):
+    def initdir(self, shared_datadir):
         self.shared_datadir = shared_datadir / 'labeling'
-        self.tmpdir = tmpdir
 
     def test_from_labeling_text_label_jsonl_to_conll2003(self):
         src_path = self.shared_datadir / 'labeling_text_label.jsonl'
-        tgt_path = self.tmpdir / 'labeling_text_label.conll2003'
-        expected = list(open(
-            self.shared_datadir / 'labeling_text_label.conll2003'
-        ))
-        d = NERDataset(filepath=src_path)
-        d.to_conll2003(tgt_path, str.split)
+        filename = 'labeling_text_label.conll2003'
+        users = defaultdict(list)
+        d = NERDataset.from_jsonl(filepath=src_path)
+        for x in d.to_conll2003(str.split):
+            users[x['user']].append(x['data'])
 
-        self.assertListEqual(list(open(tgt_path)), expected)
+        for user, data in users.items():
+            with open(self.shared_datadir / (filename + f'.user{user}')) as f:
+                expected = f.read()
+            self.assertEqual(''.join(data), expected)
 
     def test_from_labeling_jsonl_to_conll2003(self):
         src_path = self.shared_datadir / 'labeling.jsonl'
         filename = 'labeling.conll2003'
-        tgt_path = self.tmpdir / filename
-        expected = sorted(self.shared_datadir.glob(f'{filename}.*'))
+        users = defaultdict(list)
+        d = NERDataset.from_jsonl(filepath=src_path)
+        for x in d.to_conll2003(str.split):
+            users[x['user']].append(x['data'])
 
-        d = NERDataset(filepath=src_path)
-        d.to_conll2003(tgt_path, str.split)
-        for y in expected:
-            self.assertEqual(
-                (self.tmpdir / y.name).read_text(None),
-                y.read_text()
-            )
+        for user, data in users.items():
+            with open(self.shared_datadir / (filename + f'.user{user}')) as f:
+                expected = f.read()
+            self.assertEqual(''.join(data), expected)
+
+    def test_from_labeling_text_label_jsonl_to_spacy(self):
+        src_path = self.shared_datadir / 'labeling_text_label.jsonl'
+        filename = 'labeling_text_label.spacy'
+        users = defaultdict(list)
+        d = NERDataset.from_jsonl(filepath=src_path)
+        for x in d.to_spacy(str.split):
+            users[x['user']].append(x['data'])
+
+        for user, data in users.items():
+            with open(self.shared_datadir / (filename + f'.user{user}')) as f:
+                expected = json.load(f)
+            print(data)
+            self.assertEqual(data, expected)
 
     def test_from_labeling_jsonl_to_spacy(self):
-        src_path = self.shared_datadir / 'labeling_multi_users.jsonl'
-        filename = 'labeling_multi_users.spacy'
-        tgt_path = self.tmpdir / filename
-        expected = sorted(self.shared_datadir.glob(f'{filename}.*'))
+        src_path = self.shared_datadir / 'labeling.jsonl'
+        filename = 'labeling.spacy'
+        users = defaultdict(list)
+        d = NERDataset.from_jsonl(filepath=src_path)
+        for x in d.to_spacy(str.split):
+            users[x['user']].append(x['data'])
 
-        d = NERDataset(filepath=src_path)
-        d.to_spacy(tgt_path, str.split)
-        for y in expected:
-            self.assertEqual(
-                (self.tmpdir / y.name).read_text(None),
-                y.read_text()
-            )
+        for user, data in users.items():
+            with open(self.shared_datadir / (filename + f'.user{user}')) as f:
+                expected = json.load(f)
+            self.assertEqual(data, expected)
